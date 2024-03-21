2436d3ac49d14e8bbe4e7b702c4e89dc75b6d305
AndresCalvoG <andrescalvo9407@gmail.com>
2024-03-20 20:07:05 -0500

    feat: Add selected Price Add selected price <p> element to show selected product price with styles

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue';
 import ProductImageViewer from './components/ProductImageViewer.vue';
 import BreadcrumbPath from './components/BreadcrumbPath.vue';
 import ProductData from '../product-data.json';
@@ -7,6 +8,7 @@ import './style/app.scss';
 const {
   images, title, breadcrumbs,
 } = ProductData.product;
+const selectedPrice = ref(0);
 
 </script>
 
@@ -15,6 +17,7 @@ const {
   <article class="product">
     <h1 class="product-title">{{ title }}</h1>
     <ProductImageViewer :images="images" :productTitle="title"/>
+    <p class="product-price">$ {{ !isNaN(selectedPrice)?selectedPrice +'.00':0 }}</p>
   </article>
 </template>
 
